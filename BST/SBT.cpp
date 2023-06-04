67d838d4501ae4f0fba0983b6c01caefb249692c
Wfame <1511661890@qq.com>
2023-06-05 01:02:20 +0800

    add(BST):complete SBT

--- BST/SBT.cpp
+++ BST/SBT.cpp
@@ -71,3 +71,91 @@ TreeLink SBT::newnode(int v) {
     return p;
 }
 
+void SBT::ins(TreeLink &x, int v) {
+        insrt(x, v);
+}
+
+int SBT::del(TreeLink &x, int v) {
+    TreeLink p = fnd(x, v);
+    if (p == zero)
+        return 1;
+    delet(x, v);
+    return 0;
+}
+
+TreeLink SBT::fnd(TreeLink &x, int v) {
+    if (x == zero) return x;
+    if (x->value == v) return x;
+    if (v < x->value)
+        return fnd(x->lch, v);
+    else
+        return fnd(x->rch, v);
+}
+
+void SBT::insrt(TreeLink &x, int v) {
+    if (x == zero) {
+        x = newnode(v);
+        return;
+    }
+    x->size += 1;
+    if (v <= x->value)
+        insrt(x->lch, v);
+    else
+        insrt(x->rch, v);
+    maintain(x, v > x->value);
+}
+
+int SBT::delet(TreeLink &x, int v) {
+    int ret = 0;
+    x->size -= 1;
+    if (x->value == v || v < x->value && x->lch == zero || v > x->value && x->rch == zero) {
+        ret = x->value;
+        if (x->lch == zero && x->rch == zero)
+            x = zero;
+        else if (x->lch == zero && x->rch != zero)
+            x = x->rch;
+        else if (x->lch != zero && x->rch == zero)
+            x = x->lch;
+        else
+            x->value = delet(x->lch, x->value + 1);
+        return ret;
+    }
+    if (v < x->value)
+        return delet(x->lch, v);
+    else
+        return delet(x->rch, v);
+}
+
+int SBT::rank(TreeLink &x, int v) {
+    if (x == zero)
+        return 1;
+    if (v <= x->value)
+        return rank(x->lch, v);
+    else
+        return x->lch->size + 1 + rank(x->rch, v);
+}
+
+int SBT::sel(TreeLink &x, int v) {
+    if (x->lch->size + 1 == v)
+        return x->value;
+    if (x->lch->size + 1 < v)
+        return sel(x->rch, v - x->lch->size - 1);
+    else
+        return sel(x->lch, v);
+}
+
+int SBT::pre(TreeLink &x, int v) {
+    if (x == zero) return INT_MIN;
+    if (x->value >= v)
+        return pre(x->lch, v);
+    else
+        return std::max(x->value, pre(x->rch, v));
+}
+
+int SBT::suc(TreeLink &x, int v) {
+    if (x == zero) return INT_MAX;
+    if (x->value <= v)
+        return suc(x->rch, v);
+    else
+        return std::min(x->value, suc(x->lch, v));
+}
--- BST/SBT.h
+++ BST/SBT.h
@@ -8,6 +8,8 @@
 #include "node.h"
 #include <cstdio>
 #include <cstdlib>
+#include "algorithm"
+#include <climits>
 
 class SBT {
     public:
@@ -15,13 +17,19 @@ class SBT {
         TreeLink zero;
         void init();                                    //初始化SBT
         TreeLink newnode(int v);                        //新建节点
-        void del(TreeLink &x, int v);                   //删除
+        int del(TreeLink &x, int v);                    //删除
         void ins(TreeLink &x, int v);                   //插入
         TreeLink fnd(TreeLink &x, int v);               //查找
+        int rank(TreeLink &x, int v);                   //排名
+        int sel(TreeLink &x, int v);
+        int pre(TreeLink &x, int v);
+        int suc(TreeLink &x, int v);
     private:
         static void LeftRotate(TreeLink &x);            //左旋
         static void RightRotate(TreeLink &x);           //右旋
         void maintain(TreeLink &x, bool t);             //维持
+        void insrt(TreeLink &x, int v);                 //破坏树结构的插入
+        int delet(TreeLink &x, int v);             //破坏树结构的删除
 };
 
 
