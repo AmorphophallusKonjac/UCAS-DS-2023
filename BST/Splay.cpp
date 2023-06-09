31cf499ec3aa103fb16fab449ab3c5d0cdc19d18
yjkyjkyjk <2897370649@qq.com>
2023-06-09 20:38:02 +0800

    add Splay Tree

---
+++ BST/Splay.cpp
@@ -0,0 +1,211 @@
+//
+// Created by hp on 2023/6/7.
+//
+
+#include "Splay.h"
+
+void Splay_Tree::insert(TreeLink &p, int x) {
+    if(p == nullptr){
+        p = (TreeLink)malloc(sizeof(TreeNode));
+        p->lch = p->rch = p->father = nullptr;
+        p->key = x;
+        p->cnt = 1;
+        return;
+    }
+    TreeLink now = p, fa = nullptr;
+    while(1){
+        if(x == now->key){
+            now->cnt ++;
+            Splay(now);
+            return;
+        }
+        fa = now;
+        now = (x > now->key) ? now->rch : now->lch;
+        if(now == nullptr) {
+            now = (TreeLink)malloc(sizeof(TreeNode));
+            now->key = x;
+            now->cnt = 1;
+            now->father = fa;
+            now->lch = now->rch = nullptr;
+            if(x > fa->key) fa->rch = now;
+            else fa->lch = now;
+            Splay(now); return;
+        }
+    }
+}
+
+void Splay_Tree::del(TreeLink &p, int x) {
+    if(!find_rank(p, x)) return;
+    if(p->cnt > 1) {
+        p->cnt--;
+        push_up(p);
+        return;
+    }
+    if(p->lch == nullptr && p->rch == nullptr){
+        p = nullptr;
+        return;
+    }
+    if(p->rch == nullptr){
+        p = p->lch;
+        p->father = nullptr;
+        return;
+    }
+    if(p->lch == nullptr){
+        p = p->rch;
+        p->father = nullptr;
+        return;
+    }
+    TreeLink left = find_pre(p);
+    Splay(left);
+    p->rch = p->rch->rch;
+    p->rch->father = p;
+    push_up(p);
+}
+
+TreeLink Splay_Tree::find_pre(TreeLink p) {
+    TreeLink now = p->lch;
+    while(now->rch != nullptr) now = now->rch;
+    return now;
+}
+
+TreeLink Splay_Tree::find_suf(TreeLink p) {
+    TreeLink now = p->rch;
+    while(now->lch != nullptr) now = now->lch;
+    return now;
+}
+
+TreeLink Splay_Tree::find_kth(TreeLink p, int k) {
+    TreeLink now = p;
+    while(1) {
+        if (now->lch != nullptr && k <= now->lch->size) now = now->lch;
+        else {
+            int lsize = ((now->lch) ? now->lch->size : 0) + now->cnt;
+            if (k <= lsize) return now;
+            k -= lsize;
+            now = now->rch;
+        }
+    }
+}
+
+int Splay_Tree::find_rank(TreeLink p, int x) {
+    TreeLink now = p;
+    int ans = 1;
+    while(1) {
+        if(now->lch != nullptr && x < now->key) now = now->lch;
+        else{
+            ans += (now->lch) ? now->lch->size : 0;
+            if(x == now->key){ Splay(now); return ans; }
+            ans += now->cnt; now = now->rch;
+        }
+        if(now == nullptr) return 0;
+    }
+}
+
+void Splay_Tree::push_up(TreeLink x) {
+    if(x!=nullptr)
+        x->size = (x->lch ? x->lch->size : 0)
+                + (x->rch ? x->rch->size : 0) + x->cnt;
+}
+
+void Splay_Tree::rotate(TreeLink x) {
+    TreeLink y = x->father, z = y->father;
+    int w = get_w(x), w1 = get_w(y);
+    if(w==0) {  //左子树右旋
+        y->lch = x->rch; if(y->lch) y->lch->father = y;
+        x->rch = y; y->father = x;
+        x->father = z;
+        if(z != nullptr){
+            if(w1 == 0) z->lch = x;
+            else z->rch = x;
+        }
+    }
+    else {      //右子树左旋
+        y->rch = x->lch; if(y->rch) y->rch->father = y;
+        x->lch = y; y->father = x;
+        x->father = z;
+        if (z != nullptr) {
+            if (w1 == 0) z->lch = x;
+            else z->rch = x;
+        }
+    }
+    push_up(y);
+    push_up(x);
+}
+
+void Splay_Tree::Splay(TreeLink x) {
+    for(TreeLink fa = nullptr; (fa=x->father) != nullptr; rotate(x))
+            if(fa->father != nullptr)
+                    rotate((get_w(fa) == get_w(x)) ? fa : x);
+    Root = x;
+}
+
+int Splay_Tree::get_w(TreeLink x) {
+    if(x->father)
+        return x->father->rch == x;
+    else return 0;
+}
+
+void Splay_Tree::Get_inorder_traversal(TreeLink p, TreeNode *t, int i) {
+    if(p->lch) Get_inorder_traversal(p->lch, t, i);
+    t[++temp_cnt[i]] = *p;
+    if(p->rch) Get_inorder_traversal(p->rch, t, i);
+}
+
+TreeLink Splay_Tree::Build_Tree(TreeNode *t, int l, int r) {
+    if(l > r) return nullptr;
+    int mid = (l + r + 1) / 2;
+    TreeLink newNode = (TreeLink)malloc(sizeof(TreeNode));
+    newNode->key = t[mid].key;
+    newNode->cnt = t[mid].cnt;
+    newNode->lch = newNode->rch = nullptr;
+    if(l < mid) newNode->lch = Build_Tree(t, l, mid-1);
+    if(mid < r) newNode->rch = Build_Tree(t, mid+1, r);
+    push_up(newNode);
+    return newNode;
+}
+
+
+void Splay_Tree::merge(){
+    temp_cnt[0] = temp_cnt[1] = 0;
+    Get_inorder_traversal(Root->lch, temp1, 1);
+    Get_inorder_traversal(Root->rch, temp2, 2);
+    int i = 1, j = 1;
+    while(i <= temp_cnt[1] && j <= temp_cnt[2]){
+        if(temp1[i].key < temp2[j].key) {
+            temp3[i + j - 1] = temp1[i]; ++i;
+        }
+        else {
+            temp3[i + j - 1] = temp2[j]; ++j;
+        }
+    }
+    while(i <= temp_cnt[1]) {
+        temp3[i + j - 1] = temp1[i]; ++i;
+    }
+    while(j <= temp_cnt[2]) {
+        temp3[i + j - 1] = temp2[j]; ++j;
+    }
+    for(int i=1;i<=temp_cnt[1] + temp_cnt[2];++i)
+        printf("%d ",temp3[i].key);
+    puts("");
+    Root = Build_Tree(temp3, 1, temp_cnt[1] + temp_cnt[2]);
+    isSplit = 0;
+}
+
+
+TreeLink Splay_Tree::SPLIT(int x) {
+    insert(Root, x);
+    TreeLink ans = Root->rch;
+    Root->rch = nullptr;
+    push_up(Root);
+    del(Root, x);
+    return ans;
+}
+
+void Splay_Tree::split(int x){
+    TreeLink newRoot = (TreeLink)malloc(sizeof(TreeNode));
+    newRoot->rch = SPLIT(x);
+    newRoot->lch = Root;
+    Root = newRoot;
+    push_up(Root);
+    isSplit = 1;
+}
---
+++ BST/Splay.h
@@ -0,0 +1,40 @@
+//
+// Created by hp on 2023/6/7.
+//
+
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include "node.h"
+
+#ifndef BST_SPLAY_H
+#define BST_SPLAY_H
+#define MAXN 1000010
+
+
+class Splay_Tree {
+public:
+    TreeLink Root;
+    int  isSplit;
+    void insert(TreeLink &p, int x);
+    void del(TreeLink &p, int x);
+    TreeLink find_pre(TreeLink p);
+    TreeLink find_suf(TreeLink p);
+    TreeLink find_kth(TreeLink p, int k);   //查找第k大的结点
+    int find_rank(TreeLink p, int x);       //查找值为x的结点的rank,当返回值rank非0时查找成功,并把它Splay到Root
+    void split(int x);      //把两棵分裂的树作为一个虚根的左右子树
+    void merge();       //把虚根的两棵子树合并
+    TreeLink Build_Tree(TreeNode *t, int l, int r);
+private:
+    TreeNode temp1[MAXN], temp2[MAXN], temp3[MAXN];
+    int temp_cnt[3];
+    void push_up(TreeLink x);
+    void rotate(TreeLink x);
+    void Splay(TreeLink x);
+    int get_w(TreeLink x);
+    TreeLink SPLIT(int x);      //小于等于x的点保留在当前树中,大于x的点在返回值中
+    void Get_inorder_traversal(TreeLink p, TreeNode *t, int i);
+};
+
+
+#endif //BST_SPLAY_H
