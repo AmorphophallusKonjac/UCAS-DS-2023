b7bdd7606471c0932b412c5b35b5fbfcff6c508e
Wfame <1511661890@qq.com>
2023-06-09 20:44:57 +0800

    fix(BST):fix node.h

--- BST/node.h
+++ BST/node.h
@@ -8,7 +8,7 @@
 typedef struct TreeNode{
     int value, size, depth, key, cnt, bf;
     double width, x, y;                             // gui使用，注意不要占用
-    struct TreeNode *lch, *rch, *fa;
+    struct TreeNode *lch, *rch, *father;
 } TreeNode, *TreeLink;
 
 #endif //BST_NODE_H
